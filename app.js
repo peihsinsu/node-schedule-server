3494967b65014af7cae26e73952935c99d4efc5b
Simon Su <simonsu.mail@gmail.com>
2016-12-30 10:01:58 +0800

    remove useless worker implement

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var schedule = require('./routes/schedule');
 
 var app = express();
@@ -23,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
 app.use('/schedule', schedule);
 
 // catch 404 and forward to error handler
